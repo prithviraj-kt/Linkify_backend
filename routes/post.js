27b5a2a382f7e7534385dd9af592b8a7e074866b
prithvirajktagadinamani@gmail.com <prithvirajktagadinamani@gmail.com>
2022-02-09 10:15:20 +0530

    Updated post.js

--- routes/post.js
+++ routes/post.js
@@ -1,205 +1,182 @@
-const express = require("express")
-const req = require("express/lib/request")
-const router = express.Router()
+const express = require("express");
+// const req = require("express/lib/request")
+const router = express.Router();
 
-const {PrismaClient} = require("@prisma/client")
-const {user, post } = new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const { user, post } = new PrismaClient();
 
 router.post("/addpost/:username", async (req, res) => {
-    const username = req.params.username
-    const userExist = await user.findUnique({
-        where:{
-            username
-        }, select:{
-            college:true,
-            username:true,
-            id:true
-        }
-    })
+  const username = req.params.username;
+  const userExist = await user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      college: true,
+      username: true,
+      id: true,
+    },
+  });
 
-    if(!userExist){
-        return res.status(400).json({
-            msg: "User does not exist exists"
-        })
-    }
+  if (!userExist) {
+    return res.status(400).json({
+      msg: "User does not exist exists",
+    });
+  }
 
-    const {title, description} = req.body
-    const newPost = await post.create({
-        data:{
-            college:userExist.college,
-            username: userExist.username,
-            title,
-            description,
-            user_id:userExist.id
-        }
-    })
+  const { title, description } = req.body;
+  const newPost = await post.create({
+    data: {
+      college: userExist.college,
+      username: userExist.username,
+      title,
+      description,
+      user_id: userExist.id,
+    },
+  });
 
-    res.json(newPost)
-
-})
+  res.json(newPost);
+});
 
 router.put("/update/:username/:id", async (req, res) => {
-    const username = req.params.username
-    const id = parseInt( req.params.id)
+  const username = req.params.username;
+  const id = parseInt(req.params.id);
+  const userExist = await user.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (!userExist) {
+    return res.status(400).json({
+      msg: "User does not exist exists",
+    });
+  }
+  const postExist = await post.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      username: true,
+      id: true,
+    },
+  });
 
-    const userExist = await user.findUnique({
-        where:{
-            username
-        }
-    })
-
-    if(!userExist){
-        return res.status(400).json({
-            msg: "User does not exist exists"
-        })
-    }
-    // try {
-        const postExist = await post.findUnique({
-            where:{
-                id
-            }, select:{
-                username:true,
-                id:true
-            }
-        })
-    
-        if(!postExist){
-            return res.status(400).json({
-                msg: "Post does not exist exists"
-            })
-        }
-    // } catch (error) {
-    //     res.json(error)
-    // }
-    if(postExist.username === username){
-
-        const updatePost = await post.update({
-            where:{
-                id
-            },
-            data:{
-                title: req.body.title,
-                description: req.body.description,
-            }
-        })
-    
-        res.json(updatePost)
-    }else{
-        res.json({msg:"Contents cannot be changed..."})
-    }
-    
-
-})
+  if (!postExist) {
+    return res.status(400).json({
+      msg: "Post does not exist exists",
+    });
+  }
+  if (postExist.username === username) {
+    const updatePost = await post.update({
+      where: {
+        id,
+      },
+      data: {
+        title: req.body.title,
+        description: req.body.description,
+      },
+    });
+    res.json(updatePost);
+  } else {
+    res.json({ msg: "Contents cannot be changed..." });
+  }
+});
 
 router.get("/getpost/:username", async (req, res) => {
-    const username = req.params.username
-    const userExist = await user.findUnique({
-        where:{
-            username
-        }, select:{
-            college:true
-        }
-    })
+  const username = req.params.username;
+  const userExist = await user.findUnique({
+    where: {
+      username,
+    },
+    select: {
+      college: true,
+    },
+  });
 
-    if(!userExist){
-        return res.status(400).json({
-            msg: "User does not exist exists"
-        })
-    }
+  if (!userExist) {
+    return res.status(400).json({
+      msg: "User does not exist exists",
+    });
+  }
 
-    const getAllPost = await post.findMany({
-        where:{
-            college:userExist.college
-        }
-    })
+  const getAllPost = await post.findMany({
+    where: {
+      college: userExist.college,
+    },
+  });
 
-    res.json(getAllPost)
-
-})
-
-
+  res.json(getAllPost);
+});
 
 router.delete("/delete/:username/:id", async (req, res) => {
-    const username = req.params.username
-    const id = parseInt( req.params.id)
-
-
-
-    const existUser = await user.findUnique({
-        where:{
-            username
-        }
-    })
-
-    if(!existUser){
-        return res.status(400).json({
-            msg:" User does not exist"
-        })
-    }
-
-    const existPost = await post.findUnique({
-        where:{
-            id
-        }, select:{
-            username:true,
-        }
-    })
-
-    if(!existPost){
-        return res.status(400).json({
-            msg:" Post does not exist"
-        })
-    }
-
-    if(username === existPost.username){
-        const deletePost = await post.delete({
-            where:{
-                id
-            }
-        })
-        res.json({
-            deletePost, msg:"Post deleted"
-        })
-    } else{
-        res.json({
-            msg:"Cannot delete post"
-        })
-    }
-
-    // res.json({
-    //     username,id
-    // })
-})
-
+  const username = req.params.username;
+  const id = parseInt(req.params.id);
+  const existUser = await user.findUnique({
+    where: {
+      username,
+    },
+  });
+  if (!existUser) {
+    return res.status(400).json({
+      msg: " User does not exist",
+    });
+  }
+  const existPost = await post.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      username: true,
+    },
+  });
+  if (!existPost) {
+    return res.status(400).json({
+      msg: " Post does not exist",
+    });
+  }
+  if (username === existPost.username) {
+    const deletePost = await post.delete({
+      where: {
+        id,
+      },
+    });
+    res.json({
+      deletePost,
+      msg: "Post deleted",
+    });
+  } else {
+    res.json({
+      msg: "Cannot delete post",
+    });
+  }
+});
 
 router.get("/get/:username", async (req, res) => {
-    const username = req.params.username
+  const username = req.params.username;
 
-    const existUser = await user.findUnique({
-        where:{
-            username
-        }
-    })
+  const existUser = await user.findUnique({
+    where: {
+      username,
+    },
+  });
 
-    if(!existUser){
-       return res.status(400).json({
-            msg:"User does not exist"
-        })
-    }
+  if (!existUser) {
+    return res.status(400).json({
+      msg: "User does not exist",
+    });
+  }
 
-    const allPost = await post.findMany({
-        where:{
-            username
-        },select:{
-            title:true,
-            description:true
-        }
-    })
+  const allPost = await post.findMany({
+    where: {
+      username,
+    },
+    select: {
+      title: true,
+      description: true,
+    },
+  });
 
-    res.json(allPost)
+  res.json(allPost);
+});
 
-
-})
-
-
-module.exports = router
+module.exports = router;
